8573ce4e714f2a70ab06205601287ff0b6a44a97
Pedro Valentim <pedro@vlt.im>
2023-06-12 18:45:18 +0100

    Fix default template

--- templates/default/apps/web/src/components/Button.tsx
+++ templates/default/apps/web/src/components/Button.tsx
@@ -4,9 +4,9 @@ export const Button = () => {
   return (
     <div className="rounded-md ">
       <a href="https://turbo.build/repo/docs">
-        <div className="ui-flex ui-w-full ui-items-center ui-justify-center ui-rounded-md ui-border ui-border-transparent ui-px-8 ui-py-3 ui-text-base ui-font-medium ui-no-underline ui-bg-white ui-text-black hover:ui-bg-gray-300 md:ui-py-3 md:ui-px-10 md:ui-text-lg md:ui-leading-6">
+        <div className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-black no-underline hover:bg-gray-300 md:px-10 md:py-3 md:text-lg md:leading-6">
           Read the docs
-          <span className="ui-ml-2 ui-bg-gradient-to-r ui-from-brandred ui-to-brandblue ui-bg-clip-text ui-text-transparent">
+          <span className="from-brandred to-brandblue ml-2 bg-gradient-to-r bg-clip-text text-transparent">
             →
           </span>
         </div>

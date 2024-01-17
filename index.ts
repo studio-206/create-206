2b1676849f089f8bec26d8a181f7452d866f89f9
Sam Scott <samoscarscott@gmail.com>
2024-01-17 11:08:19 +0000

    .

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ const program = new Commander.Command(packageJson.name)
   .version(packageJson.version)
   .arguments("<project-directory>")
   .usage(`${chalk.green("<project-directory>")} [options]`)
-  .action((name) => {
+  .action(name => {
     projectPath = name;
   })
   .option(
@@ -45,7 +45,7 @@ const program = new Commander.Command(packageJson.name)
   An template to bootstrap the app with. You can use an template name
   from the Create 206 repo or a GitHub URL. The URL can use
   any branch and/or subdirectory
-`
+`,
   )
   .option(
     "--template-path <path-to-template>",
@@ -55,14 +55,14 @@ const program = new Commander.Command(packageJson.name)
   a slash (e.g. bug/fix-1) and the path to the template (e.g. foo/bar).
   In this case, you must specify the path to the template separately:
   --template-path foo/bar
-`
+`,
   )
   .option(
     "--reset-preferences",
     `
 
   Explicitly tell the CLI to reset any stored preferences
-`
+`,
   )
   .allowUnknownOption()
   .parse(process.argv);
@@ -70,10 +70,10 @@ const program = new Commander.Command(packageJson.name)
 const packageManager = !!program.useNpm
   ? "npm"
   : !!program.usePnpm
-  ? "pnpm"
-  : !!program.useYarn
-  ? "yarn"
-  : getPkgManager();
+    ? "pnpm"
+    : !!program.useYarn
+      ? "yarn"
+      : getPkgManager();
 
 async function run(): Promise<void> {
   const conf = new Conf({ projectName: "create-next-app" });
@@ -95,7 +95,7 @@ async function run(): Promise<void> {
       name: "path",
       message: "What is your project named?",
       initial: "my-app",
-      validate: (name) => {
+      validate: name => {
         const validation = validateNpmName(path.basename(path.resolve(name)));
         if (validation.valid) {
           return true;
@@ -112,12 +112,10 @@ async function run(): Promise<void> {
   if (!projectPath) {
     console.log(
       "\nPlease specify the project directory:\n" +
-        `  ${chalk.cyan(program.name())} ${chalk.green(
-          "<project-directory>"
-        )}\n` +
+        `  ${chalk.cyan(program.name())} ${chalk.green("<project-directory>")}\n` +
         "For example:\n" +
         `  ${chalk.cyan(program.name())} ${chalk.green("my-next-app")}\n\n` +
-        `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
+        `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`,
     );
     process.exit(1);
   }
@@ -129,18 +127,16 @@ async function run(): Promise<void> {
   if (!valid) {
     console.error(
       `Could not create a project called ${chalk.red(
-        `"${projectName}"`
-      )} because of npm naming restrictions:`
+        `"${projectName}"`,
+      )} because of npm naming restrictions:`,
     );
 
-    problems!.forEach((p) => console.error(`    ${chalk.red.bold("*")} ${p}`));
+    problems!.forEach(p => console.error(`    ${chalk.red.bold("*")} ${p}`));
     process.exit(1);
   }
 
   if (program.example === true) {
-    console.error(
-      "Please provide an example name or url, otherwise remove the example option."
-    );
+    console.error("Please provide an example name or url, otherwise remove the example option.");
     process.exit(1);
   }
 
@@ -155,12 +151,8 @@ async function run(): Promise<void> {
     process.exit(1);
   }
 
-  let template =
-    typeof program.template === "string" && program.template.trim();
-  const preferences = (conf.get("preferences") || {}) as Record<
-    string,
-    boolean | string
-  >;
+  let template = typeof program.template === "string" && program.template.trim();
+  const preferences = (conf.get("preferences") || {}) as Record<string, boolean | string>;
   /**
    * If the user does not provide the necessary flags, prompt them for which
    * template to use.
@@ -410,15 +402,15 @@ async function notifyUpdate(): Promise<void> {
         packageManager === "yarn"
           ? "yarn global add create-next-app"
           : packageManager === "pnpm"
-          ? "pnpm add -g create-next-app"
-          : "npm i -g create-next-app";
+            ? "pnpm add -g create-next-app"
+            : "npm i -g create-next-app";
 
       console.log(
         chalk.yellow.bold("A new version of `create-next-app` is available!") +
           "\n" +
           "You can update by running: " +
           chalk.cyan(updateMessage) +
-          "\n"
+          "\n",
       );
     }
     process.exit();
@@ -429,16 +421,13 @@ async function notifyUpdate(): Promise<void> {
 
 run()
   .then(notifyUpdate)
-  .catch(async (reason) => {
+  .catch(async reason => {
     console.log();
     console.log("Aborting installation.");
     if (reason.command) {
       console.log(`  ${chalk.cyan(reason.command)} has failed.`);
     } else {
-      console.log(
-        chalk.red("Unexpected error. Please report it as a bug:") + "\n",
-        reason
-      );
+      console.log(chalk.red("Unexpected error. Please report it as a bug:") + "\n", reason);
     }
     console.log();
 

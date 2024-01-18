3fa5a6d135def0d82b27bafeb7636ae557c2be27
Sam Scott <samoscarscott@gmail.com>
2024-01-18 16:13:31 +0000

    update envs

--- templates/sanity-pages/apps/web/src/sanity/client.ts
+++ templates/sanity-pages/apps/web/src/sanity/client.ts
@@ -3,9 +3,9 @@ import { createClient } from "@sanity/client/stega";
 
 export function getClient(previewToken?: string): SanityClient {
   return createClient({
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
-    apiVersion: process.env.SANITY_API_VERSION || "2022-03-07",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2022-03-07",
     useCdn: !previewToken,
     perspective: previewToken ? "previewDrafts" : "published",
     stega: {

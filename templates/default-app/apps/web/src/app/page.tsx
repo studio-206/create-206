06546f3efd45c44410140d0063abf01bc1ed7f50
Sam Scott <samoscarscott@gmail.com>
2024-01-18 12:54:08 +0000

    Make app/pages templates more similar

--- templates/default-app/apps/web/src/app/page.tsx
+++ templates/default-app/apps/web/src/app/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-zinc-900">
       <main className="flex w-auto flex-col items-center justify-center px-4 pb-8 pt-16 sm:pt-24 lg:p-8">
         <img src="/studio206.svg" className="h-10 w-auto" />
         <h1 className="my-12 text-center text-6xl font-medium uppercase tracking-wide text-white sm:text-7xl lg:text-8xl">

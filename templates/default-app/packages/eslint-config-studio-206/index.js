d9ea654c7ac681e323c55d3df88bda29b1b9fd9a
karl <karl@studio206.dev>
2024-01-29 13:04:48 +0000

    fix eslint

--- templates/default-app/packages/eslint-config-studio-206/index.js
+++ templates/default-app/packages/eslint-config-studio-206/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "@studio206/eslint-config"]
+  extends: ["next", "@studio206/eslint-config"],
   // These parserOptions fix the following issue: https://github.com/vercel/next.js/issues/40687
   parserOptions: {
     babelOptions: {

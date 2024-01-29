d4798d46200b8f45a6e25f6325fd5dbc23229066
karl <karl@studio206.dev>
2024-01-29 13:18:12 +0000

    update eslints

--- templates/default-app/.eslintrc.js
+++ templates/default-app/.eslintrc.js
@@ -7,10 +7,4 @@ module.exports = {
       rootDir: ["apps/*/"],
     },
   },
-  // These parserOptions fix the following issue: https://github.com/vercel/next.js/issues/40687
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
 };

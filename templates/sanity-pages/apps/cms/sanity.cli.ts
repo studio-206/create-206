e2b43293178e1aeafc00cbeae2775ccd22318a79
karl <karl@studio206.dev>
2024-02-01 14:55:47 +0000

    updates

--- templates/sanity-pages/apps/cms/sanity.cli.ts
+++ templates/sanity-pages/apps/cms/sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: "cqtgqlz7",
+    projectId: process.env.SANITY_PROJECT_ID,
     dataset: "production",
   },
 });

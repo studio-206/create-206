bfe61833949d445e8ba800be6933425051c72e64
Fabio Ruolo <hello@fabioruolo.com>
2026-03-02 13:44:29 +0000

    added documentation component to CMS template

--- templates/sanity-pages/apps/cms/deskStructure.ts
+++ templates/sanity-pages/apps/cms/deskStructure.ts
@@ -1,10 +1,15 @@
 import { CogIcon } from "@sanity/icons";
 import { StructureBuilder } from "sanity/structure";
+import { Documentation } from "./components/Documentation";
 
 export const deskStructure = (S: StructureBuilder) =>
   S.list()
     .title("Content")
     .items([
+      S.listItem()
+        .title("Documentation")
+        .child(S.component(Documentation).title("CMS Documentation")),
+      S.divider(),
       ...S.documentTypeListItems().filter((item) => item.getId() !== "settings"),
       S.divider(),
       S.listItem()
